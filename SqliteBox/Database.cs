b9780254f2b352108449febc0c0075c34f07c02a
JohanLarsson <johan_gavle1@hotmail.com>
2018-06-15 14:23:19 +0200

    Working db first.

---
+++ SqliteBox/Database.cs
@@ -0,0 +1,25 @@
+﻿namespace SqliteBox
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class Database : DbContext
+    {
+        private static bool _created = false;
+        public Database()
+        {
+            if (!_created)
+            {
+                _created = true;
+                this.Database.EnsureDeleted();
+                this.Database.EnsureCreated();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite(@"Data Source=test.db");
+        }
+ 
+        public DbSet<Foo> Foos { get; set; }
+    }
+}
---
+++ SqliteBox/Foo.cs
@@ -0,0 +1,9 @@
+﻿namespace SqliteBox
+{
+    public class Foo
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
